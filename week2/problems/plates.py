735f163bd944e74b5fdae60997d4ef6e9c86eaa2
Billy Farrell <billyfarrell95@gmail.com>
2025-03-28 14:16:43 -0500

    finish plates problem

--- week2/problems/plates.py
+++ week2/problems/plates.py
@@ -1,24 +1,65 @@
+from string import punctuation
 def main():
-    plate = input("Plate: ")
-    if is_valid(plate):
-        print("Valid")
-    else:
-        print("Invalid")
+    while True:
+        plate = input("Plate: ")
+        if is_valid(plate):
+            print("Valid")
+        else:
+            print("Invalid")
 
-
-def is_valid(s):
-    start_two_letter = (s[0].isalpha() and s[1].isalpha())
-    valid_len = (len(s) > 1 and len(s) < 7)
-    starts_zero = s.startswith("0")
-    nums_in_middle = False
-    for char in s:
-        if char.isnumeric() and s[-1].isalpha():
-            nums_in_middle = True
-    if (start_two_letter and valid_len and not starts_zero and not nums_in_middle):
+def does_start_two_letters(s):
+    if s[0].isalpha() and s[1].isalpha():
         return True
     else:
         return False
 
+def validate_len(s):
+    if (len(s) > 1 and len(s) < 7):
+        return True
+    else: 
+        return False
 
+def is_first_num_zero(s):
+    for c in s:
+        if c.isnumeric():
+            if int(c) > 0:
+                return True
+                break
+            else:
+                return False
+            
+def has_nums_in_middle(s):
+    for c in s:
+        if c.isnumeric() and s[-1].isalpha():
+            return False
+        else:
+            return True
+
+def has_punct(s):
+    for p in punctuation:
+        if p in s:
+            print("has punc")
+            return False
+            break
+        else:
+            return True
+
+def is_valid(s):
+    """
+    Must start with 2 letters
+    Max 6 chars
+    Min 2 chars
+    Numbers can't be used in middle
+    First num used can't be 0
+    No periods, spaces, or punctuation marks
+    """
+    is_valid_length = validate_len(s)
+    if is_valid_length:
+        start_two_letters = does_start_two_letters(s[:2])
+        first_num_not_zero = is_first_num_zero(s)
+        no_nums_in_middle = has_nums_in_middle(s[2:])
+        no_punctuation = has_punct(s)
+        if start_two_letters and first_num_not_zero and no_nums_in_middle and no_punctuation:
+            return True
 
 main()
