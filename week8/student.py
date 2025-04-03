e56c39012e5f47d20cf91c25d97ad9f4e1c15626
Billy Farrell <billyfarrell95@gmail.com>
2025-04-03 16:06:41 -0500

    progress on week8 lecture

--- week8/student.py
+++ week8/student.py
@@ -1,14 +1,44 @@
+import emoji
 
-# Lecture 19:00
+# Lecture - 1:16
+class Student:
+    # Instantiate object
+    def __init__(self, name, house, patronus):
+        if not name:
+            raise ValueError("Missing name")
+        if house not in ["Gryffindor", "Hufflepuff", "Ravenclaw", "Slytherin"]:
+            raise ValueError("Invalid house")
+        self.name = name
+        self.house = house
+        self.patronus = patronus
+    
+    def __str__(self):
+        return f"{self.name} at {self.house}"
+    
+    # By convention, should take self even if it is unused
+    def charm(self):
+        match self.patronus:
+            case "Stag":
+                return emoji.emojize(":horse_face:")
+            case "Otter":
+                return emoji.emojize(":otter:")
+            case "Jack Russell terrier":
+                return emoji.emojize(":dog_face:")
+            case _:
+                return emoji.emojize(":magic_wand:")
+
 def main():
     student = get_student()
-    if student[0] == "Padma":
-        student[1] = "Ravenclaw"
-    print(f"{student[0]} from {student[1]}")
+    print("Expecto Patronum!")
+    print(student)
+    print(student.charm())
 
 def get_student():
     name = input("Name: ")
     house = input("House: ")
-    return [name, house]
+    patronus = input("Patronus: ")
+    # Constructor call
+    return Student(name, house, patronus) 
+
 if __name__ == "__main__":
     main()
