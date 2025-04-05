2dfda37d2327c042fa4f149a2dac918b3376e4a9
Billy Farrell <billyfarrell95@gmail.com>
2025-04-05 10:57:51 -0500

    fix errors in week1 meal project/test

--- week1/problems/meal.py
+++ week1/problems/meal.py
@@ -1,49 +1,25 @@
-def main():
-    print("Formats accepted are 24h: 07:33, 7:33, 12:23 | 12h: 7:30 am, 5:30 pm")
-    time = input("What time is it? ")
-    convert(time)
+import sys
 
+def main():
+    time_input = input("What time is it? ")
+    time = convert(time_input)
+    if 7 <= time <= 8:
+        print("breakfast time")
+    elif 12 <= time <= 13:
+        print("lunch time")
+    elif 18 <= time <= 19:
+        print("dinner time")
+    else:
+        sys.exit()
 
 def convert(input_str):
-    hour = None
-    minutes = None
-    is_12_hour = "am" in input_str or "pm" in input_str
-    match is_12_hour:
-        case True:
-            input_str_list = input_str.split(" ")
-            time = input_str_list[0]
-            am_pm = input_str_list[1]
-            hour = float(time.split(":")[0])
-            minutes = float(time.split(":")[1])
-            if minutes < 60 and hour <= 12:
-                match am_pm:
-                    case "am":
-                        if ((hour == 7) or (hour == 8 and minutes == 0)):
-                            print("breakfast time")
-                    case "pm":
-                        if ((hour == 12) or (hour == 1 and minutes == 0)):
-                            print("lunch time")
-                        elif ((hour == 6) or (hour == 7 and minutes == 0)):
-                            print("dinner time")
-            else:
-                print("Enter a valid time")
-        case False:
-            # expects format 7:00, 07:45, 15:34
-            hour = float(input_str.split(":")[0])
-            minutes = float(input_str.split(":")[1])
-            if (minutes < 60 and hour < 24) or (minutes == 0 and hour == 24):
-                if ((hour == 7) or (hour == 8 and minutes == 0)):
-                    print("breakfast time")
-                elif ((hour == 12) or (hour == 13 and minutes == 0)):
-                    print("lunch time")
-                elif ((hour == 18) or (hour == 19 and minutes == 0)):
-                    print("dinner time")
-            else:
-                print("Enter a valid time")
-        case _:
-            print("Formatting error")
+    hour = float(input_str.split(":")[0])
+    minutes = float(input_str.split(":")[1])
+    time_int = hour + (minutes / 60)
+    return time_int
 
 if __name__ == "__main__":
     main()
 
+# PASS
 # check50 --local cs50/problems/2022/python/meal
